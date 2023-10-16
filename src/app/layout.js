9e1220770b130a793d4a23736c1e9f9f45fe7adc
Manolo Cabrera <viprestal@hotmail.com>
2023-10-16 13:54:20 +0200

    begon

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className='h-full bg-white'>
       <body className={inter.className}>{children}</body>
     </html>
   )
